c778bae74038383d10b05941e91c4b2eab262c01
guoqing <571054761@qq.com>
2025-12-30 16:14:08 +0800

    feat: feat

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -8,12 +8,54 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Map<String, String>> _tabList = [
+    {
+      "icon": "ic_public_home_normal.png",
+      "active_icon": "ic_public_home_active.png",
+      "text": "首页",
+    },
+    {
+      "icon": "ic_public_pro_normal.png",
+      "active_icon": "ic_public_pro_active.png",
+      "text": "分类",
+    },
+    {
+      "icon": "ic_public_cart_normal.png",
+      "active_icon": "ic_public_cart_active.png",
+      "text": "购物车",
+    },
+    {
+      "icon": "ic_public_my_normal.png",
+      "active_icon": "ic_public_my_active.png",
+      "text": "我的",
+    },
+  ];
+
+  List<BottomNavigationBarItem> _getTabBarWidget() {
+    List<BottomNavigationBarItem> list = [];
+    for (var item in _tabList) {
+      list.add(
+        BottomNavigationBarItem(
+          icon: Image.asset(
+            item["icon"]!,
+            width: 30,
+            height: 30,
+          ),
+          activeIcon: Image.asset(
+            item["active_icon"]!,
+            width: 30,
+            height: 30,
+          ),
+          label: item["text"],
+        )
+      );
+    }
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MainPage'),
-      ),
       body: Center(
         child: TextButton(
           onPressed: () {
@@ -22,6 +64,7 @@ class _MainPageState extends State<MainPage> {
           child: const Text('Login'),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: _getTabBarWidget()),
     );
   }
 }
